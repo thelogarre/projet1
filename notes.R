cd1df7ac66560d842a2414042782505bf33bb5e4
Thélo Garré <thelo@fc30671.int.fir.alliancecan.ca>
2026-07-13 07:19:03 -0700

    création du script notes

---
+++ notes.R
@@ -0,0 +1,10 @@
+#SAVEGARDER SUR GITHUB
+
+# 1. Signale à Git que tu veux sauvegarder les changements de baseline.R
+git add baseline.R
+
+# 2. Crée le point de sauvegarde avec un petit mot explicatif
+git commit -m "Mise à jour du script : ajout de graphiques ou filtres"
+
+# 3. Envoie le tout sur GitHub
+git push
